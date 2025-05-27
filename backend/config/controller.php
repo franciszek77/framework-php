fad63f3c42241dc46b0e8a629595d19373058a26
Francisco Toro <franciszek77@gmail.com>
2025-05-27 00:22:55 -0400

    Eliminar directorios frontend, backend y public que se generan con install.php

--- backend/config/controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'modules' => [
-        'login' => 'Login',
-        'usuario' => 'Usuario',
-        'example' => 'Example',
-    ],
-    'api_base' => '/framework-php/public/api/',
-];
